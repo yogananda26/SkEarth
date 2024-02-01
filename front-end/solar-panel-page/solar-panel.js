a7c66d21abaaf6b5501cc448933907e67e2f4c9c
MarvelJo <mjmarvelm@gmail.com>
2024-02-01 21:28:29 +0700

    apeng

---
+++ front-end/solar-panel-page/solar-panel.js
@@ -0,0 +1,12 @@
+function myFunctionl() {
+    var popup = document.getElementById("myPopupl");
+    popup.classList.toggle("show");
+  }
+  function myFunctionm() {
+    var popup = document.getElementById("myPopupm");
+    popup.classList.toggle("show");
+  }
+  function myFunctionr() {
+    var popup = document.getElementById("myPopupr");
+    popup.classList.toggle("show");
+  }
