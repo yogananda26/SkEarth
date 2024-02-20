80ac52d4bf4c7999c7ef6ad729ef8d1f0baf789c
JackyZhang2004 <jackyzhng2004@gmail.com>
2024-02-20 21:40:31 +0700

    modified:   front-end/chat-page/newchat.css 	modified:   front-end/chat-page/newchat.html 	modified:   front-end/chat-page/newchat.js 	new file:   front-end/property/chat/add-image.png 	new file:   front-end/property/chat/video-files.png

--- front-end/chat-page/newchat.js
+++ front-end/chat-page/newchat.js
@@ -1,23 +1,17 @@
 function dropFunction(dropdownlist){
     document.getElementById(dropdownlist).classList.toggle("show");
-    if(dropdownlist == "dropdown-list1"){
-        var remdropdown = document.getElementById("dropdown-list2");
-        var closedropdown = remdropdown;
-        if (closedropdown.classList.contains('show')) {
-            closedropdown.classList.remove('show');
-        }
-    }
-    else{
-        var remdropdown = document.getElementById("dropdown-list1");
-        var closedropdown = remdropdown;
-        if (closedropdown.classList.contains('show')) {
-            closedropdown.classList.remove('show');
-        }
+    document.getElementById("content-dropbtn").style.backgroundColor = "white";
+    document.getElementById("content-dropbtn").style.color = "rgb(32, 50, 67)";
+
+    if (dropdownlist.classList.contains('show')) {
+        document.getElementById("content-dropbtn").style.backgroundColor = "transparent";
+        document.getElementById("content-dropbtn").style.color = "white";
+        dropdownlist.classList.remove('show');
     }
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('dropbtn')) {
+    if (!event.target.matches('content-dropbtn')) {
         var dropdown = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdown.length; i++) {
